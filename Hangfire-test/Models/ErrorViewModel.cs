72d54a51faf4c718548b0f0c5ae185b472f49ad7
Ifeanyi Lawrence <ifeanyilawrence.eze@gmail.com>
2020-01-12 12:23:58 +0100

    Project Setup

---
+++ Hangfire-test/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Hangfire_test.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
